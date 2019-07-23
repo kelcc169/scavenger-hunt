1636031953a7bac1ef383cab406cf18e1b49be50
Kelsey Cox <kelseycox@Lucy.local>
2019-07-23 10:49:20 -0700

    added adventure lists and selecting

---
+++ client/src/AdventureList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const AdventureList = (props) => (
+  <div>
+    {props.lists.map( (list, index) => 
+      <div key={index}>
+        <h3>{list.name}</h3>
+        <Link to='/adventure'>
+          <button onClick={props.handleListSelect} value={list._id} > Go On An Adventure! </button>
+        </Link>
+      </div>
+    )}
+  </div>
+)
+
+export default AdventureList;
