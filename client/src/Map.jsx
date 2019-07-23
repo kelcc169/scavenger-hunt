69b1f9f0b6cca7f78ca4311865bd04ba65a5b0fe
Michael <sound.plus.fury@gmail.com>
2019-07-23 16:40:42 -0700

    Building out the map component

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,15 +1,72 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import ReactMapboxGl from 'react-mapbox-gl';
+//Added dependencies: mapbox, react-mapbox-gl-draw
 
-// Div or container that refreshes without refreshing the entire web page.
+class Map extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lng: '',
+			lat: ''
+		}
+	}
 
-const Map = (props) => {
-	<div className="Map">
-		"THIS IS A MAP"
-	</div>
+	render () {
+		return (
+			<div className="mapboxBox">
+				
+			</div>
+		)
+	}
+
+
+
+
+
+
+
+
+	render () {
+		return (
+			<>
+				<h1>This is a test. </h1> {''}{''}
+				<div className="map">
+					<p> MAP will go here-ish. </p>
+				</div>
+				<img src={this.state.pictureUrl} alt='goal'/>
+				<button onClick={this.handleButtonClick} >I'm a button</button>
+			</>
+		);	
+	}
+
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+// const Map = (props) => {
+// 	<div className="Map">
+// 		THIS IS A MAP"
+// 	</div>
+
+// }
 
 
 export default Map;
