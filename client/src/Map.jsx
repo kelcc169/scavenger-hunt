5229ed4f9692d5cfe876b96b9fc21849fde29fd3
Kelsey Cox <kelseycox@Lucy.local>
2019-07-23 19:37:27 -0700

    the map works

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,62 +1,40 @@
 import React from 'react';
-import axios from 'axios';
-import './App.css';
-import ReactMapboxGl from 'react-mapbox-gl';
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
-//Added dependencies: mapbox, react-mapbox-gl-draw
+import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
 
 class Map extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			lng: this.props.lng,
-			lat: this.props.lat
-		}
-	}
-
 	render () {
+		let lng = this.props.lng
+		let lat = this.props.lat
+
 		const Map = new ReactMapboxGl({
 			accessToken: 'pk.eyJ1IjoibWNkdWRsZXk4NyIsImEiOiJjanhlejR5YWIwdWFwM25tcHNubDdpejIwIn0.n-RmlJrsycjQ76M82M_02Q',
 			container: 'map',
-			zoom: 9
+			minZoom: 12,
+			maxZoom: 16
 		}
 	);
-	let lat = this.state.lat
-	let lng = this.state.lng
 
 	return (
 			<>
 				<div className="mapboxBox">
 					<Map
-							center = {[lng, lat]}
-							style="mapbox://styles/mapbox/streets-v9"
-							containerStyle={{
-								height: '250px',
-								width: '250px'
-							}}>
-						{/* <DrawControl /> */}
+						center={[lng, lat]}
+						style="mapbox://styles/mapbox/streets-v9"
+						minZoom={[3]}
+						maxZoom={[10]}
+						containerStyle={{
+							height: '250px',
+							width: '250px'
+						}}>
+						<Marker coordinates={[lng, lat]}
+							style={{backgroundColor: '#444', height: '25px', width: '25px', borderRadius: '50%'}}>
+						</Marker>
 					</Map>
 				</div>
-			<button onClick={this.handleButtonClick}>SOMEDAY I'LL BE A USEFUL SUBMIT BUTTON! </button>
+				<button onClick={this.handleButtonClick}>SOMEDAY I'LL BE A USEFUL SUBMIT BUTTON! </button>
 			</>
 		)
 	}
 }	
 
-
-
 export default Map;
-
-
-
-
-
-
-
-
-	// const Map = (props) => {
-	// 	<div className="Map">
-	// 		THIS IS A MAP"
-	// 	</div>
-	
-	// }
