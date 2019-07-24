c33ae52a2e175dd020f555c41e6d3f278bf577d4
Kelsey Cox <kelseycox@Lucy.local>
2019-07-24 11:00:48 -0700

    zoom and marker color

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -3,6 +3,7 @@ import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
 
 class Map extends React.Component {
 	render () {
+		// let creating = this.props.creating
 		let lng = this.props.lng
 		let lat = this.props.lat
 
@@ -10,9 +11,13 @@ class Map extends React.Component {
 			accessToken: 'pk.eyJ1IjoibWNkdWRsZXk4NyIsImEiOiJjanhlejR5YWIwdWFwM25tcHNubDdpejIwIn0.n-RmlJrsycjQ76M82M_02Q',
 			container: 'map',
 			minZoom: 12,
-			maxZoom: 16
+			maxZoom: 14
 		}
 	);
+
+	// if creating...
+		// return map with geolocation
+	// if !creating...
 
 	return (
 			<>
@@ -20,14 +25,12 @@ class Map extends React.Component {
 					<Map
 						center={[lng, lat]}
 						style="mapbox://styles/mapbox/streets-v9"
-						minZoom={[3]}
-						maxZoom={[10]}
 						containerStyle={{
 							height: '250px',
 							width: '250px'
 						}}>
 						<Marker coordinates={[lng, lat]}
-							style={{backgroundColor: '#444', height: '25px', width: '25px', borderRadius: '50%'}}>
+							style={{backgroundColor: 'rgba(100, 0, 100, 50% )', height: '25px', width: '25px', borderRadius: '50%'}}>
 						</Marker>
 					</Map>
 				</div>
